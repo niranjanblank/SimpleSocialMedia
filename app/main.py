dabd2cc1c006db9529322cd6ed21f9b53eb354da
Niranjan Shah <niranjanshah474@gmail.com>
2024-02-06 19:44:06 +1100

    CORS setting added

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.database import create_db_and_tables
 from .routers import user_router, board_router, board_list_router
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +12,15 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 # include the routers
 app.include_router(user_router.router)
 app.include_router(board_router.router)
