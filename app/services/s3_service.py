ff62515d3216497d957be5cac0704328d79482de
Niranjan Shah <niranjanshah474@gmail.com>
2024-08-23 18:31:20 +1000

    aws access keys added to the s3_service, boto3 client

--- app/services/s3_service.py
+++ app/services/s3_service.py
@@ -10,8 +10,14 @@ load_dotenv()
 BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
 DEFAULT_IMAGE_URL = os.getenv("DEFAULT_IMAGE_URL")
 REGION = os.getenv('AWS_REGION')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client('s3',
+                         aws_access_key_id=AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                         region_name=REGION
+                         )
 
 
 def upload_to_s3(file: UploadFile, object_name: str, bucket_name: str = BUCKET_NAME, region: str = REGION):
